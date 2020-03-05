97d5726dd40f404b51e6fefe29b159f8ce02983d
ItaloQoliveira <ita.lo500023@gmail.com>
2020-03-05 19:43:42 -0300

    05/04 19:43

---
+++ Poo/Bobao.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Bobao{
+    public static void main(String args[]){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Digite seu nome completo");
+        String nome= inp.next();
+        System.out.println("Não ligo pro seu sobrenome, "+ nome+", você é um bobão");
+        
+
+    }
+}
